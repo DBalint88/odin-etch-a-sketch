b48555de839121fc3a9dd2859293565b3c389625
DBalint88 <dbalint88@gmail.com>
2022-11-14 20:22:51 -0500

    Implement button to change grid size and reset

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-let sketchBox = document.getElementById("sketch-box");
+let currentSketchBox = document.getElementById("current-sketch-box");
+const densityButton = document.getElementById("density-button");
+const resetButton = document.getElementById("reset-button");
+const main = document.getElementById("main");
+let density = 16;
+let sketchCell;
 
 
-for (let i=0; i<256; i++) {
-    let newCell = document.createElement('div');
-    newCell.classList.add("sketch-cell");
-    sketchBox.appendChild(newCell);
+populateSketchBox();
+
+densityButton.addEventListener("click", getGridDensity);
+
+function getGridDensity () {
+    density = prompt("Please enter a number between 1 & 100.");
+    changeGrid(density);
+    populateSketchBox(density);
 }
 
-let sketchCell = document.getElementsByClassName('sketch-cell');
+function changeGrid (density = 16) {
+    currentSketchBox.removeAttribute("id");
+    currentSketchBox.remove();
+    currentSketchBox = document.createElement('div');
+    currentSketchBox.classList.add("sketch-box");
+    currentSketchBox.id = "current-sketch-box";
+    currentSketchBox.style.gridTemplateColumns = "repeat(" + density + ", " + 960/density + "px)";
+    currentSketchBox.style.gridTemplateRows = "repeat(" + density + ", " + 960/density + "px)"; 
+    main.appendChild(currentSketchBox);
+}
 
-for (const cell of sketchCell) {
-    cell.addEventListener("mouseover", draw);
+function populateSketchBox (density = 16) {
+    for (let i = 0; i < density*density; i++) {
+        let newCell = document.createElement('div');
+        newCell.classList.add("sketch-cell");
+        newCell.style.width = 960 / density + "px";
+        newCell.style.height = 960 / density + "px";
+        currentSketchBox.appendChild(newCell);
+    }
+    sketchCell = document.getElementsByClassName('sketch-cell');
+    for (const cell of sketchCell) {
+        cell.addEventListener("mouseover", draw);
+    }
 }
 
 function draw () {
     this.style.backgroundColor = "black";
 }
+
